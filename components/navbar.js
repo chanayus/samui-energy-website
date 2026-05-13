de56445397df31aefe3b5bcc4f3352613dca1c35
Xhanayus <chanayus1526@gmail.com>
2026-05-13 11:52:34 +0700

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -12,7 +12,7 @@ class Navbar extends HTMLElement {
       <nav id="navbar" class="w-full container z-20 fixed lg:top-8 top-4 text-black left-1/2 -translate-x-1/2">
         <div class="bg-white inner lg:px-4 lg:pr-2 px-5 py-2 rounded-full">
            <div class="flex justify-between items-center ">
-            <a href="">
+            <a href="../index.html">
               <img src="../images/logo.webp" class="lg:w-32 w-22" alt="" width="140" height="46" />
             </a>
 
